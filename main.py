50a979da6f48a18179b3d21635dd0232eee64acb
Rinee <rinee0206@gmail.com>
2025-12-10 10:46:59 +0530

    Use pyautogen 0.9.0 and autogen.AssistantAgent

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Dict, Optional
 import os
-from autogen.agentchat import AssistantAgent
+from autogen import AssistantAgent
+
 
 
 
